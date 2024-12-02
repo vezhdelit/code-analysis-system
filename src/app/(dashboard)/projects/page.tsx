d09639576b361cd305af90445b8671bee727958d
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-12-02 02:10:04 +0200

    fix

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import dashedArrowAnim from '@/assets/animations/dashed-arrow-anim.json';
-import Lottie from 'lottie-react';
+// import dashedArrowAnim from '@/assets/animations/dashed-arrow-anim.json';
+// import Lottie from 'lottie-react';
 
 export default function ProjectsPage() {
     return (
         <main className='relative flex flex-1 items-center justify-center rounded-lg bg-muted'>
-            <Lottie
+            {/* <Lottie
                 className='absolute left-2 top-12 rotate-[195deg] opacity-100'
                 animationData={dashedArrowAnim}
                 loop={false}
-            />
+            /> */}
             <span className='text-xl font-semibold text-accent-foreground/50'>
                 Choose or create a project to get started
             </span>
